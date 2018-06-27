3341980dd27a0d0454f1c204fe6eb8624ca84c26
daolinma <madaolin@gmail.com>
2018-06-27 10:46:00 -0400

    a little sleep time before moving

--- src/motionstage/src/controller_dsg.py
+++ src/motionstage/src/controller_dsg.py
@@ -119,6 +119,7 @@ def move_motor(angle = 1.5707, rot = 0):
     print(' Starting move...')
     print(' Moving to end_point for this line...')
     read_data()
+    rospy.sleep(.5)
     print('[MOTOR] Speed has been reset')
     c('BG ABC') #begin motion
     # g.GMotionComplete('ABC')
